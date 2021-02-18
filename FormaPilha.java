9055a5c5e1aa8ad60480cc4a4f605b80c15ee75e
LuanRamalho <32373552+LuanRamalho@users.noreply.github.com>
2021-02-18 14:00:50 -0300

    Add files via upload

---
+++ FormaPilha.java
@@ -0,0 +1,188 @@
+import java.util.*;
+
+public class FormaPilha 
+{
+    Scanner ler = new Scanner(System.in);
+    int w;
+    int i;
+    int y = 1;
+    int a[] = new int[11];
+    
+      
+    void LerPilha()
+    {
+        a[0] = 3;
+        a[1] = -6;
+        a[2] = 18;
+        a[3] = -16;
+        a[4] = -3;
+        a[5] = 15;
+        a[6] = 10;
+        a[7] = 4;
+        a[8] = 12;
+        a[9] = -2;
+        a[10] = 11;
+    }
+
+    void Empilhar()
+    {
+        System.out.println("");
+        System.out.println("Empilhar: ");
+        for(i=10; i>=10; i= i-1)
+        {
+            System.out.println(""+a[i]);
+        }
+        
+        System.out.println("");
+        System.out.println("Empilhar: ");
+        for(i=10; i>=9; i= i-1)
+        {
+            System.out.println(""+a[i]);
+        }
+        
+        System.out.println("");
+        System.out.println("Empilhar: ");
+        for(i=10; i>=8; i= i-1)
+        {
+            System.out.println(""+a[i]);
+        }
+        
+        System.out.println("");
+        System.out.println("Empilhar: ");
+        for(i=10; i>=7; i= i-1)
+        {
+            System.out.println(""+a[i]);
+        }
+        
+        System.out.println("");
+        System.out.println("Empilhar: ");
+        for(i=10; i>=6; i= i-1)
+        {
+            System.out.println(""+a[i]);
+        }
+        
+        System.out.println("");
+        System.out.println("Empilhar: ");
+        for(i=10; i>=5; i= i-1)
+        {
+            System.out.println(""+a[i]);
+        }
+        
+        System.out.println("");
+        System.out.println("Empilhar: ");
+        for(i=10; i>=4; i= i-1)
+        {
+            System.out.println(""+a[i]);
+        }
+        
+        System.out.println("");
+        System.out.println("Empilhar: ");
+        for(i=10; i>=3; i= i-1)
+        {
+            System.out.println(""+a[i]);
+        }
+        
+        System.out.println("");
+        System.out.println("Empilhar: ");
+        for(i=10; i>=2; i= i-1)
+        {
+            System.out.println(""+a[i]);
+        }
+        
+        System.out.println("");
+        System.out.println("Empilhar: ");
+        for(i=10; i>=1; i= i-1)
+        {
+            System.out.println(""+a[i]);
+        }
+        
+        System.out.println("");
+        System.out.println("Empilhar: ");
+        for(i=10; i>=0; i= i-1)
+        {
+            System.out.println(""+a[i]);
+        }
+    }        
+    
+    void Desempilhar()
+    {
+        System.out.println("");
+        System.out.println("Desempilhar: ");
+        for(i=10; i>=0; i= i-1)
+        {
+            System.out.println(""+a[i]);
+        }
+        
+        System.out.println("");
+        System.out.println("Desempilhar: ");
+        for(i=9; i>=0; i= i-1)
+        {
+            System.out.println(""+a[i]);
+        }
+        
+        System.out.println("");
+        System.out.println("Desempilhar: ");
+        for(i=8; i>=0; i= i-1)
+        {
+            System.out.println(""+a[i]);
+        }
+        
+        System.out.println("");
+        System.out.println("Desempilhar: ");
+        for(i=7; i>=0; i= i-1)
+        {
+            System.out.println(""+a[i]);
+        }
+        
+        System.out.println("");
+        System.out.println("Desempilhar: ");
+        for(i=6; i>=0; i= i-1)
+        {
+            System.out.println(""+a[i]);
+        }
+        
+        System.out.println("");
+        System.out.println("Desempilhar: ");
+        for(i=5; i>=0; i= i-1)
+        {
+            System.out.println(""+a[i]);
+        }
+        
+        System.out.println("");
+        System.out.println("Desempilhar: ");
+        for(i=4; i>=0; i= i-1)
+        {
+            System.out.println(""+a[i]);
+        }
+        
+        System.out.println("");
+        System.out.println("Desempilhar: ");
+        for(i=3; i>=0; i= i-1)
+        {
+            System.out.println(""+a[i]);
+        }
+        
+        System.out.println("");
+        System.out.println("Desempilhar: ");
+        for(i=2; i>=0; i= i-1)
+        {
+            System.out.println(""+a[i]);
+        }
+        
+        System.out.println("");
+        System.out.println("Desempilhar: ");
+        for(i=1; i>=0; i= i-1)
+        {
+            System.out.println(""+a[i]);
+        }
+        
+        System.out.println("");
+        System.out.println("Desempilhar: ");
+        for(i=0; i>=0; i= i-1)
+        {
+            System.out.println(""+a[i]);
+        }
+    }
+                
+    
+}
---
+++ Pilha.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+
+public class Pilha 
+{
+   
+    public static void main(String[] args) 
+    {
+       FormaPilha p = new FormaPilha();
+       p.LerPilha();
+       p.Empilhar();
+       p.Desempilhar();
+    }
+   
+}
